296bc3d844aece7cf0bae2fc5abd3f8f096ead42
huangming <1828609633@qq.com>
2019-01-16 10:16:28 +0800

    feature:新增CustomToastUtil

---
+++ lib.common/src/main/java/com/trc/android/common/util/CustomToastUtil.java
@@ -0,0 +1,305 @@
+package com.trc.android.common.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.trc.common.R;
+
+/**
+ * @author HuangMing on 2019/1/15.
+ */
+
+public class CustomToastUtil {
+
+    private Activity mActivity;
+    private RelativeLayout mToastLayout;
+    private ToastLayout mToast;
+    private ViewGroup mView;
+    private String text;
+    private long times;
+    private static CustomToastUtil mToastInstance;
+    private FrameLayout.LayoutParams mViewGroupParams;
+    /**
+     * 固定参数
+     */
+    public static final long LENGTH_LONG = 3000;
+    public static final long LENGTH_SHORT = 1000;
+    /**
+     * 静态可设置参数
+     */
+    public static int bgColor;
+    public static int textColor;
+    public static int height = 30;
+
+    /**
+     * 设置高度
+     *
+     * @param height
+     */
+    public static void setHeight(int height) {
+        CustomToastUtil.height = height;
+    }
+
+    /**
+     * 背景色
+     *
+     * @param bgColor
+     */
+    public static void setBgColor(int bgColor) {
+        bgColor = bgColor;
+    }
+
+    /**
+     * 文字颜色
+     *
+     * @param textColor
+     */
+    public static void setTextColor(int textColor) {
+        textColor = textColor;
+    }
+
+    /**
+     * 初始化
+     *
+     * @param bgColor   背景颜色
+     * @param textColor 文字颜色
+     * @param height    高度
+     */
+    public static void init(int bgColor, int textColor, int height) {
+        CustomToastUtil.bgColor = bgColor;
+        CustomToastUtil.textColor = textColor;
+        CustomToastUtil.height = height;
+    }
+
+    /**
+     * 构造函数，上下文为activity
+     *
+     * @param mActivity
+     * @param text
+     * @param times
+     */
+    public CustomToastUtil(Activity mActivity, String text, long times) {
+        this.mActivity = mActivity;
+        this.text = text;
+        this.times = times;
+    }
+
+    /**
+     * 构造函数，上下文为View
+     *
+     * @param mView
+     * @param text
+     * @param times
+     */
+    public CustomToastUtil(ViewGroup mView, String text, long times) {
+        this.mView = mView;
+        this.text = text;
+        this.times = times;
+    }
+
+    /**
+     * 调用方法，上下文为activity
+     *
+     * @param mActivity
+     * @param text
+     * @param times
+     * @return
+     */
+    public static CustomToastUtil makeText(Activity mActivity, String text, long times) {
+        mToastInstance = new CustomToastUtil(mActivity, text, times);
+        return mToastInstance;
+    }
+
+    /**
+     * 调用方法，上下文为view
+     *
+     * @param mView
+     * @param text
+     * @param times
+     * @return
+     */
+    public static CustomToastUtil makeText(ViewGroup mView, String text, long times) {
+        mToastInstance = new CustomToastUtil(mView, text, times);
+        return mToastInstance;
+    }
+
+    /**
+     * 展示
+     */
+    public void show() {
+        if (mActivity != null) {
+            initLayoutParams(mActivity);
+            mToastLayout = mActivity.findViewById(R.id.rl_toast);
+            //判断是否已经添加进母VIEW里，没有则添加进去
+            if (mToastLayout == null) {
+                mToast = new ToastLayout(mActivity);
+                initToast(mToast);
+                mActivity.addContentView(mToast, mViewGroupParams);
+            } else {
+                //如果有，则直接取出
+                mToast = (ToastLayout) mToastLayout.getParent();
+            }
+            mToast.setContent(text);
+            mToast.showToast(times);
+        } else if (mView != null) {
+            initLayoutParams(mView.getContext());
+            mToastLayout = mView.findViewById(R.id.rl_toast);
+            if (mToastLayout == null) {
+                mToast = new ToastLayout(mView.getContext());
+                initToast(mToast);
+                mView.addView(mToast, mViewGroupParams);
+            } else {
+                mToast = (ToastLayout) mToastLayout.getParent();
+            }
+            mToast.setContent(text);
+            mToast.showToast(times);
+        }
+    }
+
+    /**
+     * 初始化页面布局
+     *
+     * @param context
+     */
+    private void initLayoutParams(Context context) {
+        mViewGroupParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, ToastLayout.dip2px(context, height));
+        mViewGroupParams.gravity = Gravity.BOTTOM | Gravity.CENTER;
+        mViewGroupParams.bottomMargin = 200;
+    }
+
+    /**
+     * 设置各个参数
+     *
+     * @param mToast
+     */
+    private void initToast(ToastLayout mToast) {
+        if (textColor != 0) {
+            mToast.setTextColor(textColor);
+        }
+        if (bgColor != 0) {
+            mToast.setBgColor(bgColor);
+        }
+        mToast.setHeight(height);
+    }
+
+    private boolean isShowToast() {
+        return mToast != null && mToast.isShow();
+    }
+
+    /**
+     * 是否在展示
+     *
+     * @return
+     */
+    public static boolean isShow() {
+        if (mToastInstance == null) {
+            return false;
+        } else {
+            boolean isShow = mToastInstance.isShowToast();
+            mToastInstance = null;
+            return isShow;
+        }
+    }
+
+    static class ToastLayout extends RelativeLayout {
+        private static final int ANIMATION_TIME = 200;
+        private TextView mContent;
+        private View view;
+        private boolean isShow;
+        private RelativeLayout mWrapper;
+        private int height;
+
+        public boolean isShow() {
+            return isShow;
+        }
+
+        public ToastLayout(Context context) {
+            this(context, null);
+        }
+
+        public ToastLayout(Context context, AttributeSet attrs) {
+            this(context, attrs, 0);
+        }
+
+        public ToastLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+            view = LayoutInflater.from(getContext()).inflate(R.layout.lib_common_toast, null);
+            addView(view);
+            mContent = view.findViewById(R.id.tv_content);
+            mWrapper = view.findViewById(R.id.rl_toast);
+            height = 60;
+        }
+
+        public void setTextColor(int color) {
+            mContent.setTextColor(color);
+        }
+
+        public void setBgColor(int color) {
+            mWrapper.setBackgroundColor(color);
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+
+        public void setContent(String content) {
+            if (mContent != null) {
+                mContent.setText(content);
+            }
+        }
+
+        public void showToast(long time) {
+            AnimationSet animationSet = new AnimationSet(true);
+            TranslateAnimation trans1 = new TranslateAnimation(0, 0, -dip2px(getContext(), height), 0);
+            TranslateAnimation trans2 = new TranslateAnimation(0, 0, 0, -dip2px(getContext(), height));
+            trans1.setDuration(ANIMATION_TIME);
+            trans2.setStartOffset(ANIMATION_TIME + time);
+            trans2.setDuration(ANIMATION_TIME);
+            animationSet.addAnimation(trans1);
+            animationSet.addAnimation(trans2);
+            this.startAnimation(animationSet);
+            animationSet.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    isShow = true;
+                    ToastLayout.this.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    isShow = false;
+                    ToastLayout.this.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        }
+
+        /**
+         * 将dip或dp值转换为px值，保证尺寸大小不变
+         *
+         * @param context
+         * @param dipValue
+         * @return
+         */
+        public static int dip2px(Context context, float dipValue) {
+            float density = context.getResources().getDisplayMetrics().density;
+            return (int) (dipValue * density + 0.5f);
+        }
+    }
+}
